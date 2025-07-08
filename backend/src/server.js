73471f3d88e733c9de34f4276deccbf0917796db
Anuj Dangi <23mcce16@uohyd.ac.in>
2025-07-08 23:50:31 +0530

    intial commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
 import express from "express";
+import dotenv from "dotenv;"
 
 const app = express();
 
-app.listen(5000, () => {
+app.listen(5001, () => {
     console.log("Server is running on PORT: 5001 ");
 } );
