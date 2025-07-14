38a82ce8545759edd25a723ab3fbd1d134304573
Anuj Dangi <23mcce16@uohyd.ac.in>
2025-07-14 23:59:44 +0530

    updating server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
 import express from "express";
-import dotenv from "dotenv;"
+import ENV from "./config/env.js";
 
 const app = express();
 
-app.listen(5001, () => {
-    console.log("Server is running on PORT: 5001 ");
+app.listen(ENV.PORT, () => {
+    console.log("Server is running on PORT: ", ENV.PORT);
 } );
