7b5239d01139f2a2fc56fc7cef174fc3d47c851c
Anuj Dangi <23mcce16@uohyd.ac.in>
2025-07-15 12:43:04 +0530

    configuring database

---
+++ backend/src/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { ENV } from  "./env.js";
+
+export const connectDB = async() => {
+    try
+    {
+        await mongoose.connect(ENV.MONGO_URI);
+
+        console.log("Connected to DB Successfully ")
+    }
+    catch(error)
+    {
+        console.log("Error connecting to MONGODB");
+        process.exit(1);
+    }
+}
