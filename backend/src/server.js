aec6d209c7f659640ade4017db8d44399124992d
Anuj Dangi <23mcce16@uohyd.ac.in>
2025-07-15 12:43:45 +0530

    running database on port

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
 import express from "express";
-import ENV from "./config/env.js";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
-app.listen(ENV.PORT, () => {
-    console.log("Server is running on PORT: ", ENV.PORT);
-} );
+connectDB();
+
+app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
