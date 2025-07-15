436c5adb4949a6a745ccb861c083528c431c0e43
Anuj Dangi <23mcce16@uohyd.ac.in>
2025-07-15 16:06:29 +0530

    setting up server

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,19 @@ import { connectDB } from "./config/db.js";
 
 const app = express();
 
-connectDB();
+const startServer = async () => {
+    try
+    {
+        await connectDB();
 
-app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
+        app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
+    }
+    catch(error)
+    {
+        console.log("Failed to start server: ", error.message);
+        process.exit(1);
+    }
+}
+
+startServer();
+
