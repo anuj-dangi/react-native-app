d9f07314759055a1116fddf31abab74f97f41b57
Anuj Dangi <23mcce16@uohyd.ac.in>
2025-07-25 16:44:12 +0530

    get user profile and updateProfile controller function

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/user.model.js";
+import { getAuth } from "@clerk/express";
 
 export const getUserProfile = asyncHandler(async (req, res) => {
     const { username } = req.params;
@@ -12,4 +13,14 @@ export const getUserProfile = asyncHandler(async (req, res) => {
     }
 
     res.status(200).json({ user });
+});
+
+export const updateProfile = asyncHandler(async (req, res) => {
+    const { userId } = getAuth(req);
+
+    const user = await User.findOneAndUpdate({ clerkId: userId }, req.body, { new: true });
+
+    if(!user)   return res.status(404).json({error: "User not found"});
+
+    res.status(200).json({ user }); 
 });
