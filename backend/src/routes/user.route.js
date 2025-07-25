b7456a040f9d2887236f0e819558bd906198fa23
Anuj Dangi <23mcce16@uohyd.ac.in>
2025-07-25 20:16:04 +0530

    getUserProfile, syncUser, getCurrentUser, updateProfile, followUser routes

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,16 @@
 import express from "express";
-
-import { getUserProfile } from "../controllers/user.controller.js";
+import { getUserProfile, updateProfile, syncUser, getCurrentUser, followUser } from "../controllers/user.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js"; 
 
 const router = express.Router();
 
+//public route
 router.get("/profile/:username", getUserProfile);
+
+//protected route
+router.post("/sync", protectRoute, syncUser);
+router.get("/me", protectRoute, getCurrentUser);
+router.put("/profile", protectRoute, updateProfile);
+router.post("/follow/:targetUserId", protectRoute, followUser);
 
 export default router;
